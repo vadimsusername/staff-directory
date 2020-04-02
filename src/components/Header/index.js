028496672fb51b00c55325fd688028d5dc9226b2
vadimsusername <vadimburyakovsky@yahoo.com>
2020-04-02 19:12:06 -0400

    added header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header(){
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <h4>Sort by name or search by name</h4>
+        </div>
+    )
+}
+
+export default Header;
