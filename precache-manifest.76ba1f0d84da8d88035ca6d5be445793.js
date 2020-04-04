3297a1729d371cf7439760c47e0d48e0fcd30b82
vadimsusername <vadimburyakovsky@yahoo.com>
2020-04-03 22:37:47 -0400

    Updates

---
+++ precache-manifest.76ba1f0d84da8d88035ca6d5be445793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b35cf55770a4eb74871038416df752a",
+    "url": "/staff-directory/index.html"
+  },
+  {
+    "revision": "df1da3ac8ac56fcb22f3",
+    "url": "/staff-directory/static/css/main.37ae9b36.chunk.css"
+  },
+  {
+    "revision": "afc50165560ab3d7c316",
+    "url": "/staff-directory/static/js/2.018c9b73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/staff-directory/static/js/2.018c9b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1da3ac8ac56fcb22f3",
+    "url": "/staff-directory/static/js/main.51dacbe4.chunk.js"
+  },
+  {
+    "revision": "1ddb8bcda4ee25bd63e1",
+    "url": "/staff-directory/static/js/runtime-main.be75392e.js"
+  }
+]);
